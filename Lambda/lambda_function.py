d7267e7b3cafab193c7fa75324b740d0da5cdde2
Leon Arpio Fernandez <ie702086@iteso.mx>
2020-04-12 02:58:46 -0500

    Add: First Lambda test

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -1,6 +1,31 @@
 import json
+import paho.mqtt.client as mqtt
+
+ubidots_broker = "industrial.api.ubidots.com"
+PORT = 1883
+client_id = "LAMBDA_PUB"
+topic = "/v1.6/devices/357520077334163"
+
+message = '{"temp":10,"hum":10,"lum":10}'
+
+connect = False
+
+def on_connect(client, userdata, flags, rc):
+	global connect
+
+	connect = True
 
 def lambda_handler(event, context):
+
+	ubi_client = mqtt.Client(client_id)
+	ubi_client.username_pw_set("BBFF-mimwlgWwTC29aKimKv0jOU57SVNikeEK6J58eJqeibsoxMtNgqoFA4m")
+	ubi_client.connect(ubidots_broker, PORT)
+
+	while(!connect):
+		a = 1
+
+	ubi_client.publish(topic, message)
+
     # TODO implement
     return {
         'statusCode': 200,
