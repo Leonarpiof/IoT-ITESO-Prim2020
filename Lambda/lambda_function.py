98dc196644c7a5e4a170bc19f1f9ab9af5dfb1b3
Leon Arpio Fernandez <ie702086@iteso.mx>
2020-04-13 12:32:16 -0500

    Fix: Lambda message to Ubidots now by HTTP
    MQTT in lambda does not work properly as it only publishes once in a while

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -1,34 +1,40 @@
 import json
-import paho.mqtt.client as mqtt
+import requests
+import time
 
-ubidots_broker = "industrial.api.ubidots.com"
-PORT = 1883
-client_id = "LAMBDA_PUB"
-topic = "/v1.6/devices/357520077334163"
+TOKEN = "BBFF-mimwlgWwTC29aKimKv0jOU57SVNikeEK6J58eJqeibsoxMtNgqoFA4m"
+DEVICE_LABEL = "357520077334163"
+address = "http://industrial.api.ubidots.com"
 
-message = '{"temp":20,"hum":20,"lum":20}'
+message = {'temp':25,'hum':40,'lum':8000}
 
-connect = False
-ubi_client = mqtt.Client(client_id)
+def post_request(payload):
+	url = "{}/api/v1.6/devices/{}".format(address, DEVICE_LABEL)
+	headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
 
-def on_connect(client, userdata, flags, rc):
-	global connect
+	status = 400
+	attempts = 0
 
-	connect = True
+	while status >= 400 and attempts <= 5:
+		req = requests.post(url=url, headers=headers, json=payload)
+		status = req.status_code
+		attempts += 1
+		time.sleep(1)
+		print(status)
+
+	# Processes results
+	if status >= 400:
+		print("[ERROR] Could not send data after 5 attempts, please check \
+			\nyour token credentials and internet connection")
+		return False
+
+	print("[INFO] request made properly, your device is updated")
+	return True
 
 def lambda_handler(event, context):
-	ubi_client.on_connect = on_connect
-	ubi_client.username_pw_set("BBFF-mimwlgWwTC29aKimKv0jOU57SVNikeEK6J58eJqeibsoxMtNgqoFA4m")
-	ubi_client.connect(ubidots_broker, PORT)
-
-	ubi_client.loop_start()
-
-	while not connect:
-		a = 1
-
-	ubi_client.publish(topic, message)
-
-	ubi_client.disconnect()
+	post_request(message)
 
     # TODO implement
 	return {'statusCode': 200, 'body': json.dumps('Hello from Lambda!')}
+
+lambda_handler("test", "test")
