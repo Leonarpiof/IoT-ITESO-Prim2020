a3ef64da0d081d7251818d3eefd6e2ba7c23473d
Leon Arpio Fernandez <ie702086@iteso.mx>
2020-04-13 23:52:57 -0500

    Lambda according to Arduino message

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -1,11 +1,10 @@
 import json
 import requests
 import time
+import boto3
 
 # Ubidots access token
 TOKEN = "BBFF-mimwlgWwTC29aKimKv0jOU57SVNikeEK6J58eJqeibsoxMtNgqoFA4m"
-# Device's label
-DEVICE_LABEL = "357520077334163"
 # Ubidots stem address
 address = "http://industrial.api.ubidots.com"
 
@@ -17,9 +16,9 @@ hum_pswd = 0x6FA
 lum_pswd = 0xC37B8
 
 # Posts via HTTP to ubidots
-def post_request(payload):
+def post_request(payload, device_label):
 	# sets the address and the label to the URL
-	url = "{}/api/v1.6/devices/{}".format(address, DEVICE_LABEL)
+	url = "{}/api/v1.6/devices/{}".format(address, device_label)
 	# Headers for the post request
 	headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
 
@@ -48,12 +47,19 @@ def post_request(payload):
 
 # Lambda function handler
 def lambda_handler(event, context):
+	# Dictionary to be sent to Ubidots with error values
+	msg_to_be_sent = {"temp":{"value": -1, "timestamp": 0}, "hum":-1, "lum":-1}
+
 	event["temp"] ^= temp_pswd
 	event["hum"] ^= hum_pswd
 	event["lum"] ^= lum_pswd
 
+	msg_to_be_sent["temp"] = {"value":event["temp"], "timestamp":event["timestamp"]}
+	msg_to_be_sent["hum"] = event["hum"]
+	msg_to_be_sent["lum"] = event["lum"]
+
 	# Publishes the message to ubidots via HTTP
-	post_request(event)
+	post_request(msg_to_be_sent, event["IMEI"])
 
     # Return value
-	return {'statusCode': 200, 'body': json.dumps('Hello from Lambda!')}
+	return(str(event))
